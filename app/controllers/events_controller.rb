7f5c835168801edac47fc72842cabd41cc8bae40
Scott Atkins <satkins16@gmail.com>
2017-09-29 03:03:24 +0000

    event show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,22 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    event = Event.create(event_params)
+    event.user = current_user
+    event.save
+    redirect_to event_path(event)
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date)
+  end
+
 end
