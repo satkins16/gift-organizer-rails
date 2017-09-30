9b6803098048b30106fe2fc13047851fc4f86ebb
Scott Atkins <satkins16@gmail.com>
2017-09-30 23:57:12 +0000

    NEED TO PERMIT GIVER_IDS

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -5,9 +5,14 @@ class GiftsController < ApplicationController
   end
 
   def create
+    @event = Event.find(params[:event_id])
     gift = Gift.create(gift_params)
+    giver_params.each do |id|
+      gift.givers << Giver.find(id)
+    end
+    @event.gifts << gift.save
 
-
+    redirect_to event_path(@event)
   end
 
 
@@ -15,6 +20,10 @@ private
 
   def gift_params
     params.require(:gift).permit(:name)
+  end
+
+  def giver_params
+    params.require(:gift).permit(:giver_ids)
   end
 
 end
