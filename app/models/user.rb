5002c2e72a5597577974e0bb5a878995d9b31de8
Scott Atkins <satkins16@gmail.com>
2017-10-11 23:53:29 +0000

    fixed omniauth issues, need to get name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   has_many :gifts
   has_many :givers
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
 end
