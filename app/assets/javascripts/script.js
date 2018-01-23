b9559f6b0404c47940f6aa2d7ef4defc391db79f
satkins16 <satkins16@gmail.com>
2018-01-23 18:45:34 -0500

    fixed load event form issue

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,15 @@
+$(function () {
+  $('#new_gift').submit(function(event) {
+    //prevent form from submitting the default way
+    event.preventDefault();
+    debugger
+
+    //var values = $(this).serialize();
+
+    //var posting = $.post('/posts', values);
+
+    //posting.done(function(data) {
+      // TODO: handle response
+    //});
+  });
+});
